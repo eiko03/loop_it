a2c4085d090b9b5f6baa73dbb301bfd57649b0a1
Tipu Sultan Eiko <tipusultan50@gmail.com>
2022-01-14 10:23:45 +0600

    Update Filter.php

--- Modules/Filter.php
+++ Modules/Filter.php
@@ -7,7 +7,7 @@ abstract class Filter{
     public function handle($request , Closure $next){
         $builder = $next( $request);
 
-        if(request()->has(['sort','sort_by']))
+        if(request()->has(['sort','sort_by']) || request()->has('search'))
             return $this->applyFilter($builder);
 
         return $builder;
