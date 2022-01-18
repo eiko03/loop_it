2d94e9aa91bebf368bd03d25f62a834f48613973
eiko-iq <tsultan@iquantile.com>
2022-01-18 11:20:16 +0600

    added testcase

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,7 +3,15 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\Authentication\Models\User;
 use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+enum Auth: string
+{
+    case email="admin@gmail.com";
+    case password="123Qaw@!!9Io09ZZ";
+}
 
 class ExampleTest extends TestCase
 {
@@ -12,9 +20,58 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_home()
     {
-        $response = $this->get('/');
+        $response = $this->json('GET', 'api/no-url');
+
+        $response->assertStatus(404);
+    }
+
+    public function test_unauthenticated()
+    {
+        $response = $this->json('GET', '/api/auth/me/');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_login()
+    {
+        $baseUrl = "/api/auth/login";
+
+        $response = $this->json('POST', $baseUrl . '/', [
+            'email' => Auth::email,
+            'password' => Auth::password
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'access_token', 'expires_in'
+            ]);
+    }
+
+    public function testLogout()
+    {
+        $user = User::where('email', Auth::email)->first();
+        $token = JWTAuth::fromUser($user);
+        $baseUrl = '/api/auth/logout?token=' . $token;
+
+        $response = $this->json('POST', $baseUrl, []);
+
+        $response
+            ->assertStatus(200)
+            ->assertExactJson([
+                'message' => 'Successfully logged out'
+            ]);
+    }
+
+    public function test_GetCars()
+    {
+        $user = User::where('email', Auth::email)->first();
+        $token = JWTAuth::fromUser($user);
+        $baseUrl = '/api/car?token=' . $token;
+
+        $response = $this->json('GET', $baseUrl . '/', []);
 
         $response->assertStatus(200);
     }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,43 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Modules\Authentication\Models\User;
+use Modules\Car\Models\Car;
+use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+enum Auth: string
+{
+    case email="admin@gmail.com";
+}
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+
     public function test_example()
     {
         $this->assertTrue(true);
+    }
+
+    public function test_carSort()
+    {
+        $first_car=Car::OrderBy('id','ASC')->first();
+        $user = User::where('email', Auth::email)->first();
+        $token = JWTAuth::fromUser($user);
+        $baseUrl = '/api/car?token=' . $token.'&sort=ASC&sort_by=id';
+        $response = $this->json('GET', $baseUrl . '/');
+        $this->assertEquals($first_car->id,$response->getData()->data[0]->id);
+    }
+
+    public function test_carSearch()
+    {
+        $first_car=Car::OrderBy('id','ASC')->where(function ($query)  {
+            $query->where('model','LIKE', '%' .'a'. '%')->orWhere('brand','LIKE', '%' .'a'. '%');
+        })->first();
+        $user = User::where('email', Auth::email)->first();
+        $token = JWTAuth::fromUser($user);
+        $baseUrl = '/api/car?token=' . $token.'&search=a&sort=ASC&sort_by=id';
+        $response = $this->json('GET', $baseUrl . '/');
+        $this->assertEquals($first_car->id,$response->getData()->data[0]->id);
     }
 }
